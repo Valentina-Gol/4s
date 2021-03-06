6321e271518c8271c5dfcfdf93734ba0c864a9ea
Valentina-Gol <vip.valya.golubeva@mail.ru>
2021-03-06 21:37:44 +0300

    piaa is working

---
+++ piaa/l1/Square.cpp
@@ -0,0 +1,13 @@
+#include "Square.h"
+
+int Square::GetXCoordinate(){
+    return this->x_coordinate;
+}
+
+int Square::GetYCoordinate(){
+    return this->y_coordinate;
+}
+
+int Square::GetSize(){
+    return this->size;
+}
