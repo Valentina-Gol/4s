458e343aabee9b32a7f45d5ce546e189d28fda20
Valentina-Gol <vip.valya.golubeva@mail.ru>
2021-03-08 15:35:41 +0300

    add current output to lab1 in piaa

--- piaa/l1/all.cpp
+++ piaa/l1/all.cpp
@@ -23,11 +23,7 @@ public:
     bool CheckAllMatrix(int** matrix, int matrix_size, int& x_coordinate, int& y_coordinate, std::vector<Square>& vector, int vector_size);
     void PrintMatrix(int** matrix, int marix_size);
     void PrintVector(std::vector<Square> vector, int vector_size);
-<<<<<<< HEAD
     void PopBackVector(int** matrix, int matrix_size, std::vector<Square>& vector, int& vector_size, int& flag);
-=======
-    void PopBackVector(int** matrix, int matrix_size, std::vector<Square>& vector, int& vector_size);
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
     int FindNewSizeRightDown(int** matrix, int matrix_size, int x_coordinate, int y_coordinate);
     void SetSquareOfOnes(int** matrix, int matrix_size, int x_coordinate, int y_coordinate, int square_size);
     void SetSquareOfZeros(int** matrix, int matrix_size, int x_coordinate, int y_coordinate, int square_size);
@@ -51,15 +47,18 @@ int Handler::FindNewSizeRightDown(int** matrix, int matrix_size, int x_coordinat
     int i = x_coordinate, j = y_coordinate;
 
     int max_size = matrix_size-1;
-
+    
     while (right_size <= max_size && j+right_size<matrix_size && matrix[i][j+right_size]!=1 ){
         right_size++;
     } 
     while (down_size <= max_size && i+down_size<matrix_size && matrix[i+down_size][j]!=1 ){
         down_size++;
     }
+    int size=std::min(right_size, down_size);
+    std::cout<<"Find new square at coordinates: ("<<x_coordinate<<", "<<y_coordinate<<")... Find a square size: "<<size<<"\n";
 
-    return std::min(right_size, down_size);
+
+    return size;
 }
 
 void Handler::SetSquareOfOnes(int** matrix, int matrix_size, int x_coordinate, int y_coordinate, int square_size){
@@ -81,50 +80,27 @@ void Handler::SetSquareOfZeros(int** matrix, int matrix_size, int x_coordinate, 
 }
 
 bool Handler::CheckAllMatrix(int** matrix, int matrix_size, int& x_coordinate, int& y_coordinate, std::vector<Square>& vector, int vector_size){
-
-
-
-    /*for (int i=0;i<vector_size;i++){
-        if ()
-    }*/
-
-<<<<<<< HEAD
-    /*if (!vector.empty() && vector[vector_size-1].GetSize()+vector[vector_size-1].GetYCoordinate()<matrix_size && matrix[vector[vector_size-1].GetXCoordinate()][vector[vector_size-1].GetYCoordinate()+vector[vector_size-1].GetSize()]!=1){
-        x_coordinate = vector[vector_size-1].GetXCoordinate();
-        y_coordinate = vector[vector_size-1].GetYCoordinate()+vector[vector_size-1].GetSize();
-        return true;
-    }
-
-    int k=vector[vector_size-1].GetXCoordinate();
-    if (matrix[vector[vector_size-1].GetXCoordinate()][vector[vector_size-1].GetYCoordinate()+vector[vector_size-1].GetSize()]==matrix_size){
-        k+=1;
-    }
-    */
-
+    std::cout<<"Cheking free space in matrix...\n";
     for (int i=0;i<matrix_size;i++){
         for (int j=0;j<matrix_size;j++){
-=======
-    for (int i=0;i<matrix_size;i++){
-        for (int j=0;j<matrix_size;j++){
-
-
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
             if (matrix[i][j]==0){
                 x_coordinate = i;
                 y_coordinate = j;
+                std::cout<<"Find free space at coordinates: ("<<x_coordinate<<", "<<y_coordinate<<")\n";
                 return true;
             }
         }
     }
+    std::cout<<"Free space didn't find\n";
     return false;
 }
 
-<<<<<<< HEAD
 void Handler::PopBackVector(int** matrix, int matrix_size, std::vector<Square>& vector, int& vector_size, int& flag){
    
    while (1){
        if (vector_size<=3){
            flag=1;
+           std::cout<<"All optimal candidate for filling square was find, stoped cheking\n";
            break;
         } else{
             Square square = vector[vector_size-1];
@@ -144,60 +120,8 @@ void Handler::PopBackVector(int** matrix, int matrix_size, std::vector<Square>& 
                 this->SetSquareOfZeros(matrix, matrix_size, square.GetXCoordinate(), square.GetYCoordinate(), square.GetSize());
                 continue;
             }
-       }
-
-
-   }
-   
-   
-   
-   /*Square square;
-    while (!vector.empty() && square.GetSize()<=2 && vector_size >=3 ){
-        if (vector_size==3){
-            flag = 1;
-            break;
         }
-
-=======
-void Handler::PopBackVector(int** matrix, int matrix_size, std::vector<Square>& vector, int& vector_size){
-   Square square;
-    while (!vector.empty() && square.GetSize()<=2 ){
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
-        if (vector_size==1 && vector[0].GetSize()==2){
-            vector.pop_back();
-            vector.push_back(Square(matrix_size,0,0));
-            break;
-        }
-<<<<<<< HEAD
-=======
-        //std::cout<<"\nDELETE Vector: ";
-        //this->PrintVector(vector, vector_size);
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
-
-        square = vector[vector_size-1];
-        vector.pop_back();
-        vector_size-=1;
-        this->SetSquareOfZeros(matrix, matrix_size, square.GetXCoordinate(), square.GetYCoordinate(), square.GetSize());
-<<<<<<< HEAD
-        if (square.GetSize()>=2  && vector_size >3){
-=======
-        if (square.GetSize()>=2){
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
-            this->SetSquareOfOnes(matrix, matrix_size, square.GetXCoordinate(), square.GetYCoordinate(), square.GetSize()-1);
-            vector.push_back(Square(square.GetSize()-1, square.GetXCoordinate(), square.GetYCoordinate()));
-            vector_size+=1;
-            break;
-        }
-<<<<<<< HEAD
-        else if (vector_size==3){
-            flag=1;
-            break;
-
-        }
-    }*/
-=======
     }
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
 }
 
 void Handler::PrintMatrix(int** matrix, int matrix_size){
@@ -230,14 +154,9 @@ int main(){
         std::cout<<'4'<<"\n";
         std::cout<<"1 1 1\n1 2 1\n2 1 1\n2 2 1\n";
         return 0;
-
     }
 
-<<<<<<< HEAD
     if (n%2==0){
-=======
-    if (n%4==0){
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
         result_size = 4;
         finish.push_back(Square(n/2, 0, 0));
         finish.push_back(Square(n/2, 0, n/2));
@@ -246,18 +165,12 @@ int main(){
     }
     else{
 
-<<<<<<< HEAD
-=======
-    
-        current.push_back(Square(n-1, 0, 0));
-        
-
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
         int** matrix = new int*[n];
 
         for (int i=0; i<n; i++){
             matrix[i]= new int[n];
         }
+
         for (int i=0;i<n;i++){
             for (int j=0;j<n;j++){
                 matrix[i][j]=0;
@@ -265,25 +178,23 @@ int main(){
         }
 
         Handler handler;
-<<<<<<< HEAD
         if (n==15){
-        current.push_back(Square(n*2/3, 0, 0));
-        current.push_back(Square(n/3, 0, n*2/3));
-        current.push_back(Square(n/3, n*2/3,0));
+            current.push_back(Square(n*2/3, 0, 0));
+            current.push_back(Square(n/3, 0, n*2/3));
+            current.push_back(Square(n/3, n*2/3,0));
 
-        handler.SetSquareOfOnes(matrix, n, 0, 0, n*2/3);
-        handler.SetSquareOfOnes(matrix, n, 0, n*2/3, n/3);
-        handler.SetSquareOfOnes(matrix, n, n*2/3, 0, n/3);
+            handler.SetSquareOfOnes(matrix, n, 0, 0, n*2/3);
+            handler.SetSquareOfOnes(matrix, n, 0, n*2/3, n/3);
+            handler.SetSquareOfOnes(matrix, n, n*2/3, 0, n/3);
+        }
+        else{
+            current.push_back(Square(n/2 + 1, 0, 0));
+            current.push_back(Square(n/2, 0, n/2+1));
+            current.push_back(Square(n/2, n/2+1,0));
 
-       
-        }else{
-        current.push_back(Square(n/2 + 1, 0, 0));
-        current.push_back(Square(n/2, 0, n/2+1));
-        current.push_back(Square(n/2, n/2+1,0));
-
-        handler.SetSquareOfOnes(matrix, n, 0, 0, n/2+1);
-        handler.SetSquareOfOnes(matrix, n, 0, n/2+1, n/2);
-        handler.SetSquareOfOnes(matrix, n, n/2+1, 0, n/2);
+            handler.SetSquareOfOnes(matrix, n, 0, 0, n/2+1);
+            handler.SetSquareOfOnes(matrix, n, 0, n/2+1, n/2);
+            handler.SetSquareOfOnes(matrix, n, n/2+1, 0, n/2);
         }
 
         int current_x = 0;
@@ -295,52 +206,24 @@ int main(){
 
         while (!current.empty() && flag!=1){
 
+            std::cout<<"\nCurrent path: ";
+            handler.PrintVector(current, current_max_size);
+            
+            std::cout<<"\nCurrent matrix:\n";
+            handler.PrintMatrix(matrix, n);  
+
             if (n>11 && current_max_size>=n){
                 handler.PopBackVector(matrix, n, current, current_max_size, flag);
                 continue;
             }
 
             if (current_max_size>=result_size){
+                std::cout<<"Current path size is longer then finish path size, stop cheking further\n";
                 handler.PopBackVector(matrix, n, current, current_max_size, flag);
                 continue;
             }
             
-            /*std::cout<<"\nVector: ";
-            handler.PrintVector(current, current_max_size);
-            std::cout<<"Matrix:\n";
-            std::cout<<"Current coordinates: "<<current_x<<' '<< current_y<<'\n';
-            handler.PrintMatrix(matrix, n);
-            */
-
-
-
             if (handler.CheckAllMatrix(matrix, n, current_x, current_y, current, current_max_size)){
-=======
-        handler.SetSquareOfOnes(matrix, n, 0,0,n-1);
-
-        int current_x = 0;
-        int current_y = n-1;
-        int current_max_size = 1;
-        int new_square_size = 0;
-        
-
-
-        while (!current.empty() && current[0].GetSize()<n){
-
-            if (n>11 && current_max_size>=n){
-                handler.PopBackVector(matrix, n, current, current_max_size);
-                continue;
-
-            }
-
-            if (current_max_size>=result_size){
-                handler.PopBackVector(matrix, n, current, current_max_size);
-                continue;
-                //if (handler.CheckAllMatrix(matrix, n, current_x, current_y)){}
-            }
-
-            if (handler.CheckAllMatrix(matrix, n, current_x, current_y)){
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
                 int new_size = handler.FindNewSizeRightDown(matrix, n, current_x, current_y);
                 current.push_back(Square(new_size, current_x, current_y));
                 current_max_size+=1;
@@ -349,25 +232,17 @@ int main(){
             else{
                 finish = current;
                 result_size = current_max_size;
-<<<<<<< HEAD
                 handler.PopBackVector(matrix, n, current, current_max_size, flag);
-=======
-                handler.PopBackVector(matrix, n, current, current_max_size);
->>>>>>> b3018e7ba35b37079925513e80223ab4fa873e3c
             }
-
-            
         }
-            
-
         for (int i=0; i<n; i++){
             delete [] matrix[i];
         }
         delete [] matrix;
-
     }
 
-    std::cout<<result_size<<"\n";
+    
+    std::cout<<"\nMinimal result size: "<< result_size<<"\nCoordinates & Sizes of result squares: \n";
     for (int i=0;i<result_size;i++){
         std::cout<<finish[i].GetXCoordinate()+1<<' '<<finish[i].GetYCoordinate()+1<<' '<<finish[i].GetSize()<<'\n';
     }
